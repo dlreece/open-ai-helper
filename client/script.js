f2dd9b5a47350a64c8d58b06e48cefafff9a1c93
David Reece <dlreece@gmail.com>
2023-01-02 12:00:44 -0600

    added server link

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv)
 
     // fetch data from server => get bot's response
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://codex-open-ai-jxpm.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
